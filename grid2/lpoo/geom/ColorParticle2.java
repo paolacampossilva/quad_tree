78597525865d4fd5fcb5d1259bf919d806001dff
valencsoares <pvcampossoares558@gmail.com>
2026-06-21 13:55:22 -0400

    add: create ColorParticle2 class

---
+++ grid2/lpoo/geom/ColorParticle2.java
@@ -0,0 +1,25 @@
+package lpoo.geom;
+
+/**
+ * @author Valentina Campos Soares
+ */
+public class ColorParticle2 extends Particle2
+{
+    public int r;
+    public int g;
+    public int b;
+
+    public ColorParticle2(float x, float y, int r, int g, int b)
+    {
+        super(x, y);
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    @Override
+    public String toString()
+    {
+        return super.toString() + String.format(" RGB(%d,%d,%d)", r, g, b);
+    }
+}
