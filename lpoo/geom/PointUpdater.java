602ded9ebffe4735c6f87693bc9ba7a7ffea6247
Paola Campos da Silva <paa.campossilva@gmail.com>
2026-06-21 22:10:52 -0400

    add: PointUpdater.java

---
+++ lpoo/geom/PointUpdater.java
@@ -0,0 +1,13 @@
+package lpoo.geom;
+
+import java.util.*;
+
+/**
+ *
+ * @author Paulo Pagliosa
+ */
+public interface PointUpdater<P extends Point2> 
+{
+  boolean update(P[] points, float dt);
+
+} // PointUpdater
